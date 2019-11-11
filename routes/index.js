834f16daaa2d9cd936d11bac1f706e5b14e8c6ee
chhavi-kapur <kapurc1@student.douglascollege.ca>
2019-11-11 15:08:48 -0800

    Storing orders, sessions  and routing

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,7 @@ router.get('/shopping-cart', function(req, res, next) {
     res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice})
 });
 
-router.get('/checkout', function(req, res, next){
+router.get('/checkout', isLoggedIn, function(req, res, next){
     if(!req.session.cart) {
       return res.redirect('/shopping-cart');
     }
@@ -51,7 +51,7 @@ router.get('/checkout', function(req, res, next){
     res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
 });
 
-router.post('/checkout', function(req, res, next) {
+router.post('/checkout', isLoggedIn, function(req, res, next) {
       if(!req.session.cart) {
         return res.redirect('/shopping-cart');
       }
@@ -88,6 +88,13 @@ router.post('/checkout', function(req, res, next) {
 
 module.exports = router;
 
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) {
+      return next();
+  }
+  req.session.oldUrl = req.url;
+  res.redirect('/user/signin');
+}
 
 /*
 const connectionString = 'mongodb+srv://myuser:abcd1234@custore-authl.mongodb.net/test?retryWrites=true&w=majority';
--- routes/user.js
+++ routes/user.js
@@ -24,10 +24,19 @@ router.get('/signup', function(req, res, next) {
   });
   
   router.post('/signup', passport.authenticate('local.signup', {
-    successRedirect: '/user/profile',
     failureRedirect: '/user/signup',
     failureFlash: true
-  }));
+  }), function(req, res, next) {
+    if (req.session.oldUrl) {
+      var oldUrl = req.session.oldUrl;
+      req.session.oldUrl = null;
+      res.redirect(oldUrl);
+        
+    }
+    else {
+        res.redirect('/user/profile');
+    }
+});
 
   router.get('/signin', function(req, res, next) {
     var messages= req.flash('error');
@@ -35,10 +44,18 @@ router.get('/signup', function(req, res, next) {
   });
   
   router.post('/signin', passport.authenticate('local.signin', {
-    successRedirect: '/user/profile',
     failureRedirect: '/user/signin',
     failureFlash: true
-  }));
+  }), function(req, res, next) {
+      if (req.session.oldUrl) {
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+      }
+      else {
+          res.redirect('/user/profile');
+      }
+  });
 
   module.exports = router;
 
